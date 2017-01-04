dd60f03753d647e6c8ea9656562c24d76fbf7f59
Andrew McBrearty <andrew.mcbrearty@hoaresbank.co.uk>
2017-01-04 16:16:39 +0000

    Added aerospike db support to Login handler
    Added aerospike support to the Login handler to create the initial
    user with preset balance in a preset currency.

--- src/main/java/mir/interview/backend/handler/LoginHandler.java
+++ src/main/java/mir/interview/backend/handler/LoginHandler.java
@@ -2,6 +2,11 @@ package mir.interview.backend.handler;
 
 import static ratpack.jackson.Jackson.json;
 
+import java.util.UUID;
+
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
@@ -13,10 +18,22 @@ public class LoginHandler implements Handler {
 
     @Override
     public void handle(Context ctx) throws Exception {
+        String uuid = UUID.randomUUID().toString();
 
         String token = JWT.create()
+            .withClaim("uuid", uuid)
             .withIssuer("amcbrearty")
             .sign(Algorithm.HMAC256("secret"));
+
+        AerospikeClient client = new AerospikeClient("172.28.128.3", 3000);
+
+        // Namespace change from test in the aerospace configuration is preferred
+        Key accountKey = new Key("test", "account", uuid);
+        Bin accountBalance = new Bin("balance", 2000);
+        Bin accountCurrency = new Bin("currency", "GBP");
+
+        client.put(null, accountKey, accountBalance, accountCurrency);
+        client.close();
 
         ctx.byMethod(method -> method.post(() -> ctx.render(json(new Token(token)))));
     }
--- src/test/java/mir/interview/backend/handler/LoginHandlerTest.java
+++ src/test/java/mir/interview/backend/handler/LoginHandlerTest.java
@@ -3,24 +3,49 @@ package mir.interview.backend.handler;
 import static io.restassured.path.json.JsonPath.with;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.Key;
+import com.aerospike.client.Record;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ratpack.test.embed.EmbeddedApp;
 
 public class LoginHandlerTest {
 
+    private AerospikeClient aerospikeClient;
+
+    @Before
+    public void connectToAerospikeClient() {
+        aerospikeClient = new AerospikeClient("172.28.128.3", 3000);
+    }
+
+    @After
+    public void closeAerospikeClient() {
+        aerospikeClient.close();
+    }
+
     @Test
     public void testLoginPost() throws Exception {
-
         EmbeddedApp.fromHandler(new LoginHandler())
             .test(httpClient -> {
                 String token = with(httpClient.post().getBody().getText()).getString("token");
 
                 assertNotNull("Token not found in login response", token);
-                JWT.require(Algorithm.HMAC256("secret")).withIssuer("amcbrearty").build().verify(token);
+                String uuid = JWT.require(Algorithm.HMAC256("secret")).withIssuer("amcbrearty").build().verify(token).getClaim("uuid").asString();
+
+                Key key = new Key("test", "account", uuid);
+                Record record = aerospikeClient.get(null, key);
+
+                assertTrue("Balance not created", record.bins.containsKey("balance"));
+                assertTrue("Currency not created", record.bins.containsKey("currency"));
+                assertEquals("Initial balance incorrect", "2000", record.bins.get("balance").toString());
+                assertEquals("Initial currency incorrect", "GBP", record.bins.get("currency"));
             });
     }
 
